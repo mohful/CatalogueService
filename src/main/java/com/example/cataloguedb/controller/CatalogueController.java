3488cfa8b54dec6944c23d5dbc216cb18e77a579
Mohammed Ahmed Fulwala <mohammed.fulwala@hotmail.com>
2023-04-06 00:26:12 -0400

    fixed some bugs

--- src/main/java/com/example/cataloguedb/controller/CatalogueController.java
+++ src/main/java/com/example/cataloguedb/controller/CatalogueController.java
@@ -1,9 +1,10 @@
 package com.example.cataloguedb.controller;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.*;
+
 import com.example.cataloguedb.model.Catalogue;
 import com.example.cataloguedb.repository.JDBCConnection;
 import org.json.simple.JSONArray;
@@ -39,7 +40,7 @@ public class CatalogueController {
             record.put("id", rs.getInt("id"));
             record.put("name", rs.getString("name"));
             record.put("description", rs.getString("description"));
-            record.put("image", rs.getString("image"));
+            record.put("image_data", rs.getBytes("image_data"));
             array.add(record);
         }
         jsonObject.put("Catalogue", array);
@@ -50,22 +51,51 @@ public class CatalogueController {
     @PostMapping(path = "/post", consumes = "application/json")
     public String postToDatabase(@RequestBody Catalogue catalogue) throws SQLException {
         createConnection();
-        Statement statement = connection.createStatement();
-        String insert = "INSERT INTO Catalogue (name, description, image) VALUES (" + "'" + catalogue.getName() + "'" + ", " + "'" + catalogue.getDescription() + "'" + ", " + "'" + catalogue.getImage() + "'" + ");";
-        statement.executeUpdate(insert);
-        connection.close();
-        return "Item " + catalogue.getName() + " has been added to the database";
+        try (PreparedStatement statement = connection.prepareStatement("INSERT INTO Catalogue (name, description, image_data) VALUES (?, ?, ?)")) {
 
+            File imageFile = new File(catalogue.getImage());
+            InputStream imageStream = new FileInputStream(imageFile);
+
+            statement.setString(1, catalogue.getName());
+            statement.setString(2, catalogue.getDescription());
+            statement.setBinaryStream(3, imageStream);
+
+            int rowsInserted = statement.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("Image stored successfully!");
+            } else {
+                System.out.println("Failed to store image.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Item " + catalogue.getName() + " has been added to the database";
     }
 
     @PutMapping(path="/put/{id}")
     public String putToDatabase(@PathVariable Integer id, @RequestBody Catalogue catalogue) throws SQLException {
         createConnection();
-        Statement statement = connection.createStatement();
-        String update = "UPDATE Catalogue SET name=" + "'" + catalogue.getName() + "'" + ", description=" + "'" + catalogue.getDescription() + "'" + ", image=" + "'" + catalogue.getImage() + "'" + " WHERE id=" + id + ";";
-        statement.executeUpdate(update);
-        connection.close();
-        return "Item " + id + " has been updated";
+        System.out.println(catalogue);
+        String query = "UPDATE Catalogue SET name=?, description=?, image_data=? WHERE id=?";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            File imageFile = new File(catalogue.getImage());
+            InputStream imageStream = new FileInputStream(imageFile);
+
+            statement.setString(1, catalogue.getName());
+            statement.setString(2, catalogue.getDescription());
+            statement.setBinaryStream(3, imageStream);
+            statement.setInt(4, id);
+
+            int rowsUpdated = statement.executeUpdate();
+            if (rowsUpdated > 0) {
+                System.out.println("Image updated successfully!");
+            } else {
+                System.out.println("Failed to update image.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Item " + catalogue.getName() + " has been updated to the database whose id is: " + id;
     }
 
     @DeleteMapping(path="/delete/{id}")
